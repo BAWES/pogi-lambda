d97cb9d27f69d79cde47551f057b3e36a3a83a15
Khalid <khalid@bawes.net>
2018-09-20 14:14:27 +0300

    Adjust the publish command

--- publish.sh
+++ publish.sh
@@ -1,6 +1,13 @@
 # Delete zipped functions file
 #rm index.zip
 
+# Deploy ProcessEmails function
+rm processEmailsFromSendgrid.zip
+cd processEmailsFromSendgrid
+zip -X -R ../processEmailsFromSendgrid.zip *
+cd ..
+aws lambda update-function-code --function-name processEmailsFromSendgrid --zip-file fileb://processEmailsFromSendgrid.zip
+
 # Deploy testZip function
 rm testZip.zip
 cd testZip
