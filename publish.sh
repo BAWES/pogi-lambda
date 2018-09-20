f488d3601f35b6e79f97a0ccfade2f9e23ca21c2
Khalid <khalid@bawes.net>
2018-09-20 14:10:51 +0300

    Adjust publish command and gitignore to skip zip archives

--- publish.sh
+++ publish.sh
@@ -2,6 +2,8 @@
 #rm index.zip
 
 # Deploy testZip function
-rm testZipDeploy.zip
-zip -X -R testZipDeploy.zip testZip/*
-aws lambda update-function-code --function-name testZip --zip-file fileb://testZipDeploy.zip
+rm testZip.zip
+cd testZip
+zip -X -R ../testZip.zip *
+cd ..
+aws lambda update-function-code --function-name testZip --zip-file fileb://testZip.zip
