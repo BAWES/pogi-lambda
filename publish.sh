41e50f28cc02d71eb092287cfcd02dc4f0fa4daf
Khalid <khalid@bawes.net>
2018-09-20 15:18:54 +0300

    Modify publish script to delete zip package on completion

--- publish.sh
+++ publish.sh
@@ -6,11 +6,11 @@ rm processEmailsFromSendgrid.zip
 cd processEmailsFromSendgrid
 zip -X -R ../processEmailsFromSendgrid.zip *
 cd ..
-aws lambda update-function-code --function-name processEmailsFromSendgrid --zip-file fileb://processEmailsFromSendgrid.zip
+aws lambda update-function-code --function-name processEmailsFromSendgrid --zip-file fileb://processEmailsFromSendgrid.zip && rm processEmailsFromSendgrid.zip
 
 # Deploy testZip function
 rm testZip.zip
 cd testZip
 zip -X -R ../testZip.zip *
 cd ..
-aws lambda update-function-code --function-name testZip --zip-file fileb://testZip.zip
+aws lambda update-function-code --function-name testZip --zip-file fileb://testZip.zip && rm testZip.zip
