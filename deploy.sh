24c5ab09cd96e5d60fd1494619bfbdb0e8862c25
Khalid <khalid@bawes.net>
2018-09-21 12:59:26 +0300

    Additional attempt to make node modules function

--- deploy.sh
+++ deploy.sh
@@ -2,15 +2,14 @@
 #rm index.zip
 
 # Deploy ProcessEmails function
-rm processEmailsFromSendgrid.zip
 cd processEmailsFromSendgrid
 zip -X -R ../processEmailsFromSendgrid.zip *
 cd ..
 aws lambda update-function-code --function-name processEmailsFromSendgrid --zip-file fileb://processEmailsFromSendgrid.zip && rm processEmailsFromSendgrid.zip
 
 # Deploy testFunction function
-rm testFunction.zip
 cd testFunction
+chmod -R 777 .
 zip -X -R ../testFunction.zip *
 cd ..
 aws lambda update-function-code --function-name testFunction --zip-file fileb://testFunction.zip && rm testFunction.zip
--- testFunction/index.js
+++ testFunction/index.js
@@ -5,46 +5,11 @@
 var Busboy = require('busboy');
 var YError = require('yerror');
 var getRawBody = require('raw-body');
-const getBody = (content, headers) => new Promise((resolve, reject) => {
-  const filePromises = [];
-  const data = {};
-  const parser = new Busboy({'headers': headers});
 
+const busboy = new Busboy({
+		        headers: {
+		            'content-type': 'text/plain',
+		        }
+		    });
 
-  parser.on('field', (name, value) => {
-    data[name] = value;
-  });
-  parser.on('file', (name, file, filename, encoding, mimetype) => {
-    data[name] = {
-      filename,
-      encoding,
-      mimetype,
-    };
-    filePromises.push(
-      getRawBody(file).then(rawFile => (data[name].content = rawFile))
-    );
-  });
-  parser.on('error', err => reject(YError.wrap(err)));
-  parser.on('finish', () =>
-    resolve(Promise.all(filePromises).then(() => data))
-  );
-  parser.write(content);
-  parser.end();
-});
-
-
-exports.handler = (event, context, callback) => {
-
-  // Get POST requests received via SendGrid
-  let SUBJECT = event['subject'];
-
-    var response = {
-        "statusCode": 200,
-        "body": "success",
-        "isBase64Encoded": false,
-        "headers": {
-            "my_header": "my_value"
-        }
-    };
-    callback(null, response);
-};
+console.log(busboy);
