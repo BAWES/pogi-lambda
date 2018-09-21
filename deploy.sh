0c1bed71ab0dda790913041fb9c864fdfe6509df
Khalid <khalid@bawes.net>
2018-09-21 13:20:13 +0300

    Fix issue with deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,13 +3,14 @@
 
 # Deploy ProcessEmails function
 cd processEmailsFromSendgrid
-zip -X -R ../processEmailsFromSendgrid.zip *
+chmod -R ugo+r .
+zip -r ../processEmailsFromSendgrid.zip . -x "*.DS_Store"
 cd ..
 aws lambda update-function-code --function-name processEmailsFromSendgrid --zip-file fileb://processEmailsFromSendgrid.zip && rm processEmailsFromSendgrid.zip
 
 # Deploy testFunction function
 cd testFunction
-chmod -R 777 .
-zip -X -R ../testFunction.zip *
+chmod -R ugo+r .
+zip -r ../testFunction.zip . -x "*.DS_Store"
 cd ..
 aws lambda update-function-code --function-name testFunction --zip-file fileb://testFunction.zip && rm testFunction.zip
--- testFunction/index.js
+++ testFunction/index.js
@@ -1,15 +1,26 @@
 'use strict';
 
+exports.handler = (event, context, callback) => {
 
-// Function to Get Form Body
-var Busboy = require('busboy');
-var YError = require('yerror');
-var getRawBody = require('raw-body');
+    // Function to Get Form Body
+    var Busboy = require('busboy');
+    var YError = require('yerror');
+    var getRawBody = require('raw-body');
 
-const busboy = new Busboy({
-		        headers: {
-		            'content-type': 'text/plain',
-		        }
-		    });
+    const busboy = new Busboy({
+    		        headers: {
+    		            'content-type': 'text/plain',
+    		        }
+    		    });
 
-console.log(busboy);
+
+    var response = {
+        "statusCode": 200,
+        "body": "success",
+        "isBase64Encoded": false,
+        "headers": {
+            "my_header": "my_value"
+        }
+    };
+    callback(null, response);
+};
